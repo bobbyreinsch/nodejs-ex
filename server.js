3899494c1999fc34b43ee490d9cba863609b19bf
Bobby Reinsch <bobby@lineage.solutions>
2017-02-14 14:11:23 -0600

    added todo functions to server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express(),
     eps     = require('ejs'),
-    morgan  = require('morgan');
-    
+    morgan  = require('morgan'),
+    mongoose = require('mongoose'),
+    swig    = require('swig');
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
@@ -58,6 +60,66 @@ var initDb = function(callback) {
   });
 };
 
+
+// define model =================
+var Todo = mongoose.model('Todo', {
+   text : String,
+   tags: {type: [], index: true },
+   priority: Number
+});
+
+// social sharing - serve og optimized page
+
+var bot_router = express.Router();
+bot_router.get('/todo/:id',function(req,res){
+  var page_url = req.protocol + '://' + req.get('host') + req.url;
+  var _id = req.params.id;
+
+  var page_title = 'Test Title'; // this page title
+  var img_url = '/img.jpg'; // page image
+  var page_priority = '6';
+  var page_tags = 'tags,tags,many tags';
+
+  if(!_id){id=0}
+
+  Todo.find(function(err,todos){
+    if (err)
+        res.send(err);
+    console.log(todos);
+    res.render('bots',{
+      img: img_url,
+      title: todos[_id].priority,
+      description: todos[_id].text,
+      url: page_url,
+      tags: todos[_id].tags
+    });
+
+  });
+});
+
+
+app.use(function(req,res,next){
+   var ua = req.headers['user-agent'];
+    if(/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(ua)){
+     console.log(ua, ' gets sent elsewhere.');
+     bot_router(req,res,next);
+    }else{
+      next();
+    }
+});
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+
+
+// routes ======================================================================
+
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -90,6 +152,65 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
+
+
+
+// api ---------------------------------------------------------------------
+// get all todos
+app.get('/api/todos', function(req, res) {
+
+    // use mongoose to get all todos in the database
+    Todo.find(function(err, todos) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+
+        res.json(todos); // return all todos in JSON format
+    });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+    // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+        text : req.body.text,
+        tags : req.body.tags.split(),
+        priority: req.body.priority,
+        done : false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+
 
 // error handling
 app.use(function(err, req, res, next){
